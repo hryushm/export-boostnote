d645bc7e25f165d278dbb59f700af551eeda21a5
kuroshichimi <kuroshichimi03@gmail.com>
2016-08-20 19:04:04 +0900

    Update export-boostnote.py
    Add arguments --help and --date

--- export-boostnote.py
+++ export-boostnote.py
@@ -3,7 +3,21 @@ import sys
 import json
 import re
 import os.path
+from argparse import ArgumentParser
 from datetime import datetime as dt
+
+#arguments setting
+usage = 'Usage: ./export-boostnote.py [--date]'
+parser = ArgumentParser(usage=usage)
+parser.add_argument('path_to_data', \
+        action='store', \
+        nargs=None, \
+        help='Path to Boostnote data to export (data.json)' )
+parser.add_argument('-d', '--date', \
+        action='store_true', \
+        default=False, \
+        help='export markdown file name with created date. e.g. "yymmdd_FILENAME"')
+args = parser.parse_args()
 
 MDDIR = './markdown/'
 SNDIR = './snippets/'
@@ -14,14 +28,16 @@ if not os.path.exists(MDDIR):
 if not os.path.exists(SNDIR):
     os.mkdir(SNDIR)
 
-notes = json.load(open(sys.argv[1]))['notes']
+notes = json.load(open(args.path_to_data))['notes']
 
 for note in notes :
-    datetime = re.match(DATE_PATTERN, note['createdAt'])
-    ymd = dt.strptime(datetime.group(), '%Y-%m-%d').strftime('%y%m%d')
 
     if note['type'] == 'MARKDOWN_NOTE':
-        filename = ymd + '_' + note['title'] + '.md'
+        filename = note['title'] + '.md'
+        if args.date:
+            datetime = re.match(DATE_PATTERN, note['createdAt'])
+            ymd = dt.strptime(datetime.group(), '%Y-%m-%d').strftime('%y%m%d')
+            filename = ymd + '_' + filename
         print(filename)
         open(MDDIR + filename, 'w').write(note['content'])
 
